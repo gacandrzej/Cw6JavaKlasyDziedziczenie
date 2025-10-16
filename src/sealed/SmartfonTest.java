33fd6f19638dd7a53cd088ae22062f90e2e47ed0
Andrzej Gac <gac.andrzej@gmail.com>
2025-10-16 17:42:12 +0200

    add sealed and final

---
+++ src/sealed/SmartfonTest.java
@@ -0,0 +1,31 @@
+package sealed;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class SmartfonTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getProducent() {
+    }
+
+    @Test
+    void setProducent() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void włacz() {
+    }
+}
